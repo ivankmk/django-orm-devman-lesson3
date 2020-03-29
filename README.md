93b0c472b31a37b380fc44c091451f9f6e2be30a
IvanKMK <kumeyko@hotmail.com>
2020-03-29 19:33:32 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# How to adjust school journal
+
+```fix_marks(full_name)``` - based on input name, function replacing 2 or 3 point degree with 5;
+```remove_chastisements(full_name)```  - based on input name, removing all assigned chastisements;
+```create_commendation(full_name, subject)``` - based on input name and desired subject, script is adding the random commendation into the last lesson;
+
+If script couldn't locate inserted name, or the name is assigned to multiply rows, respected alerts will be printed. 
+
+Interaction with the script via Django shell:
+```python manage.py shell```
+
+# Examples
+
+First step is to load all necessary functions:
+```from scripts import fix_marks, remove_chastisements, create_commendation```
+
+Then, you can run fuctions:
+```fix_marks('Калинина Марфа')```
+output:
+```263 оценок исправлено для ученика Калинина Марфа.```
+
+# Notes
+The code is written for educational purposes - this is a lesson in the Python and web development course at [Devman] (https://dvmn.org).
---
+++ scripts.py
@@ -0,0 +1,59 @@
+from datacenter.models import Schoolkid, Mark, \
+                              Lesson, Commendation, Chastisement
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from datacenter.commendations import commendations
+import random
+
+
+def get_random_commendation():
+    return random.choice(commendations)
+
+
+def get_schoolkid(full_name):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=full_name)
+    except ObjectDoesNotExist:
+        print('There is no kid with name {}'.format(full_name))
+        return None
+    except MultipleObjectsReturned:
+        print('There are multiple kids with name {}'.format(full_name))
+        return None
+    return schoolkid
+
+
+def fix_marks(full_name):
+    schoolkid = get_schoolkid(full_name)
+    bad_marks = Mark.objects.filter(
+        points__in=[2, 3], schoolkid=schoolkid)
+    i = 0
+    for bad_mark in bad_marks:
+        bad_mark.points = 5
+        bad_mark.save()
+        i += 1
+    print('{} оценок исправлено для ученика {}.'.format(i, full_name))
+
+
+def remove_chastisements(full_name):
+    schoolkid = get_schoolkid(full_name)
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
+    print('Удалены все замечания для ученика {}.'.format(full_name))
+
+
+def create_commendation(full_name, subject):
+    schoolkid = get_schoolkid(full_name)
+    if not schoolkid:
+        return None
+    latest_lesson = Lesson.objects.filter(
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter,
+        subject__title=subject).order_by('date')[0]
+    Commendation.objects.create(
+        text=get_random_commendation(),
+        created=latest_lesson.date,
+        schoolkid=schoolkid,
+        subject=latest_lesson.subject,
+        teacher=latest_lesson.teacher)
+
+
+if __name__ == "__main__":
+    pass
